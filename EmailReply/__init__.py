0fde4a56a70638e949dd01bb6d56bc3a48d1fbc6
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2024-08-23 22:01:27 +0500

    fix LLM using search tool incorrectly (wrapped it around langchain_community.tools.Tool to give custom description)

--- EmailReply/__init__.py
+++ EmailReply/__init__.py
@@ -1,5 +1,5 @@
 from langchain_groq.chat_models import ChatGroq
-from langchain_community.tools import DuckDuckGoSearchRun
+from langchain_community.tools import DuckDuckGoSearchRun, Tool
 from dotenv import load_dotenv
 import os
 
@@ -8,6 +8,11 @@ load_dotenv()
 
 GROQ_LLM = ChatGroq(model=os.environ['LLM_MODEL'])
 search_tool = DuckDuckGoSearchRun()
+search_tool = Tool(
+    name="search_tool",
+    description="A search tool used to query DuckDuckGo for search results when trying to find information from the internet. Only pass a string as input.",
+    func=search_tool.run
+)
 
 
 EMAIL = """HI there,
