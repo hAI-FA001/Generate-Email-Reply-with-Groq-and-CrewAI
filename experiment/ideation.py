c4e78d15c58e84b14ddc0e8b47b854b1dd836b34
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2024-08-23 21:49:15 +0500

    experiment - make Crew (final)

--- experiment/ideation.py
+++ experiment/ideation.py
@@ -126,3 +126,39 @@ class EmailTasks():
             agent=agent
         )
 
+EMAIL = """HI there, \n
+I am emailing to say that the resort weather was way to cloudy and overcast. \n
+I wanted to write a song called 'Here comes the sun but it never came'
+
+What should be the weather in Arizona in April?
+
+I really hope you fix this next time.
+
+Thanks,
+George
+"""
+
+
+agents = EmailAgents()
+tasks = EmailTasks()
+
+categorizer_agent = agents.make_categorizer_agent()
+researcher_agent = agents.make_researcher_agent()
+email_writer_agent = agents.make_email_writer_agent()
+
+categorize_email_task = tasks.categorize_email(categorizer_agent)
+research_info_task = tasks.research_info_for_email(researcher_agent, categorize_email_task)
+draft_email_task = tasks.draft_email(email_writer_agent, categorize_email_task, research_info_task)
+
+crew = Crew(
+    agents=[categorizer_agent, researcher_agent, email_writer_agent],
+    tasks=[categorize_email_task, research_info_task, draft_email_task],
+    verbose=True,
+    process=Process.sequential,
+    full_output=True,
+    share_crew=False,
+)
+
+results = crew.kickoff(inputs={"email_content": EMAIL})
+print(f"\n\n\nCrewAI Results: {results}")
+print(f"Usage Metrics: {crew.usage_metrics}")
